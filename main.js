9ba3ceb2e1a176340e05133abc1adb5b27c2e4ef
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-06-03 14:22:39 -0300

    03/06

--- main.js
+++ main.js
@@ -14,3 +14,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 =new Date ("2025-09-10T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
