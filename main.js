4a5339d7d9a2ac96e9fec5e28f2c49c2835d2eac
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-06-10 14:15:31 -0300

    10/06

--- main.js
+++ main.js
@@ -15,8 +15,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 =new Date ("2025-09-10T00:00:00");
+const tempoObjetivo2 =new Date ("2025-07-23T00:00:00");
+const tempoObjetivo3 =new Date ("2025-12-17T00:00:00");
+const tempoObjetivo4 =new Date ("2025-12-10T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -31,4 +35,5 @@ function calculaTempo(tempoObjetivo) {
     horas %=24;
 
     return(dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+    
 }
